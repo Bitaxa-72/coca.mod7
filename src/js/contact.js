0564cb0d78b1cc067aa80ff83bc0e602c072965e
Vitaly <vitaly.butenev@gmail.com>
2025-07-01 20:07:06 +0300

    Deploy

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,8 +1,9 @@
-import '../scss/pricing.scss'
+import '../scss/contact.scss';
 import {useTheme} from './components/theme';
 import {useBurger} from './components/burger';
-import {usePlans} from './components/plans';
+import { usePhone } from './components/phone.js';
+
 
 useTheme();
 useBurger();
-usePlans();
+usePhone();
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import '../assets/scss/main.scss';
+import '../scss/main.scss';
 import {useTheme} from './components/theme';
 import {useBurger} from './components/burger';
 import { useInsightSlider } from './components/slider';
